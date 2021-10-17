e8bcb9455a66bfed24a0f6490ff7f526498ac3e0
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-10-17 16:09:54 +0800

    Update for better coding method
    All functions that match a non-terminal will just return it succeed or
    not. The matched int value or string will be passed out via parameter of
    reference.

--- riscv_parser.c
+++ riscv_parser.c
@@ -12,16 +12,21 @@ char buf[BUF_SZ];  // buffer to store the content of a line
 
 FILE *fp;
 
-void next();
-void title();
-int hex();
-char *lbl_name();
+int title();
 INS *ins();
-void op();
-void arg_list();
-void arg();
-char *astring();
+int arg_list();
+int lbl();
+int lbl_name(char **name);
+
+
+/*
+ * Function that matches terminals. Return 1 when match successfully, 0
+ * otherwise. The related value is passed out via parameter of reference
+ */
+int hex(int *val);
+int astring(char **str);
 int match(char c);
+void next();
 
 void next() {
   while (*p == ' ' || *p == '\t')
@@ -41,58 +46,71 @@ void next() {
   }
 }
 
-void title() {
-  int val = hex();
-  if (!tk_len) return;  // Failed reading a hex
-  next();
+int title() {
+  int val;
+  char *name;
 
-  char *name = lbl_name();
-  if (!name) return;
-  next();
-
-  if (!match(':')) return;
-  next();
-
-  if (!match('\n')) return;
-  next();
-
+  if (!hex(&val))
+    return 0;
+  if (!lbl_name(&name))
+    return 0;
+  if (!match(':'))
+    return 0;
+  if (!match('\n'))
+    return 0;
   printf("%d with name %s\n", val, name);
+  return 1;
 }
 
-int hex() {
-  int val = 0; 
+INS *ins() {
+  int addr, argc;
+  char *op, *name, **argv;
+  if (!hex(&addr))
+    return NULL;
+  if (!match(':'))
+    return NULL;
+  if (!hex(NULL))
+    return NULL;
+  if (!astring(&op))
+    return NULL;
+  if (!arg_list())
+    return NULL;
+  if (!lbl())
+    return NULL;
+  return NULL;
+}
+
+int hex(int *val) {
+  *val = 0;
 
   while (*p) {
     if (*p >= '0' && *p <= '9')
-      val = val * 16 + (*p - '0');
+      *val = *val * 16 + (*p - '0');
     else if (*p >= 'a' && *p <= 'f')
-      val = val * 16 + (*p - 'a' + 10);  // 0xa is 10 in dec
+      *val = *val * 16 + (*p - 'a' + 10);  // 0xa is 10 in dec
     else
       break;
 
     ++p;
   }
 
-  tk_len = p - tk;
-  return val;
+  if (p == tk) return 0;
+
+  next();
+  return 1;
 }
 
-char *lbl_name() {
-  char *name;
-
-  if (!match('<')) return NULL;
-  next();
-
-  name = astring();
-  if (!name) return NULL;
-  next();
-
-  if (!match('>')) return NULL;
-
-  return name;
+int lbl_name(char **name) {
+  if (!match('<'))
+    return 0;
+  if (!astring(name))
+    return 0;
+  if (!match('>'))
+    return 0;
+  return 1;
 }
 
-char *astring() {
+int astring(char **str) {
   // Find the string w/o delimiter
   char *s;
 
@@ -100,15 +118,21 @@ char *astring() {
     ++p;
 
   int len = p - tk;
-  s = malloc(len + 1);
-  strncpy(s, tk, len);
-
-  return s;
+  if (!len) {
+    *str = NULL;
+    return 0;
+  }
+  *str = malloc(len + 1);
+  strncpy(*str, tk, len);
+  next();
+  return 1;
 }
 
 int match(char c) {
-  if (*tk != c) return 0;
+  if (*tk != c)
+    return 0;
   ++p;
+  next();
   return 1;
 }
 
@@ -122,16 +146,16 @@ INS* riscv_parse(char* filename, int *ret_sz) {
   p = buf;
   next();
 
-  title();
+  if(!title()) {
+    printf("File: %s contains no title\n", filename);
+  }
 
-  /*
   while (fgets(buf, BUF_SZ, fp)) {
     INS *i = ins();
     if (!i) continue;
 
-    assemble i
+    // assemble i
   }
-  */
 
   return NULL;
 }
