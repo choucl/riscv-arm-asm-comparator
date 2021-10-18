c59f6ff2cbbb39ce7eacb787283e2bd75fd6aa4c
choucl <clc25600@gmail.com>
2021-10-18 12:56:44 +0800

    Add findbb() params, ret_bb_sz

--- main.c
+++ main.c
@@ -11,16 +11,15 @@ int main(int argc, char** argv) {
   char* filenames[2];
   char** subnames[2];
   int fcount = 0;
-  // int ret_sz = 0;
 
   for (int i = 0; i < 2; ++i) {
     filenames[i] = argv[i + 1];
     subnames[i] = split_routine(filenames[i], i, &fcount);
     for (int j = 0; j < fcount; ++j) { 
-      int ins_arr_sz, *bb_sz;
+      int ins_arr_sz, bb_count, *bb_sz;
       INS** ins_arr = (i == 0)? riscv_parse(subnames[i][j], &ins_arr_sz) :
           aarch_parse(subnames[i][j], &ins_arr_sz);
-      INS*** bb = findbb(ins_arr, ins_arr_sz, &bb_sz);
+      INS*** bb = findbb(ins_arr, ins_arr_sz, &bb_count, &bb_sz);
     }
   }
   return 0;
