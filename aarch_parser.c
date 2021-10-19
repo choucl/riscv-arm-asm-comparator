f29b45953851a1a2eaf392eb142fbd4d61417b34
choucl <clc25600@gmail.com>
2021-10-19 17:51:17 +0800

    Add transition table to classify ins type

--- aarch_parser.c
+++ aarch_parser.c
@@ -3,6 +3,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+int op_trans_tb[13][26] = {{-1}};
+
+void trans_tb_init() {
+  for (int i = 0; i < 13; ++i)
+    for (int j = 0; j < 26; ++j)
+      op_trans_tb[i][j] = -1;
+  op_trans_tb[0][0] = 7;    // A
+  op_trans_tb[0][1] = 1;    // B
+  op_trans_tb[0][2] = 11;   // C
+  op_trans_tb[0][11] = 4;   // L
+  op_trans_tb[0][12] = 9;   // M
+  op_trans_tb[0][14] = 10;  // O
+  op_trans_tb[0][18] = 2;   // S
+  op_trans_tb[2][19] = 3;   // ST
+  op_trans_tb[2][20] = 6;   // SU
+  op_trans_tb[4][3] = 5;    // LD
+  op_trans_tb[4][18] = 6;   // LS
+  op_trans_tb[7][3] = 8;    // AD
+  op_trans_tb[7][13] = 6;   // AN
+  op_trans_tb[8][3] = 6;    // ADD
+  op_trans_tb[9][14] = op_trans_tb[9][20] = 6; // MO, MU
+  op_trans_tb[10][17] = 6;  // OR
+  op_trans_tb[11][2] = 11;  // CC
+  op_trans_tb[11][12] = 12; // CM
+  op_trans_tb[12][15] = 6;  // CMP
+
+  for (int i = 0; i < 26; ++i) {
+    op_trans_tb[1][i] = 1; // BR
+    op_trans_tb[3][i] = 3; // ST
+    op_trans_tb[5][i] = 5; // LD
+    op_trans_tb[6][i] = 6; // AR
+  }
+}
+
+int findtype(char* op) {
+  int i = 0;
+  int state = 0;
+  while (op[i] != '\0' && state != -1) {
+    state = op_trans_tb[state][op[i++] - 'a'];
+    if (state == 1) return BR;
+    if (state == 3) return ST;
+    if (state == 5) return LD;
+    if (state == 6) return AR;
+  }
+  return OW;
+}
+
 char** arg_parse(char* arg_str, int* argc) {
   char** arg_list = malloc(sizeof(char*) * 6);
   int count = 0;
@@ -49,6 +96,8 @@ INS** aarch_parse(char* filename, int* ret_sz) {
   int ret_len = 16;
   INS** ret_ins = malloc(sizeof(INS*) * ret_len);
 
+  trans_tb_init();
+
   while (fgets(line, 128, f) != NULL) {
     if (ins_count == -1) {
       ins_count++;
@@ -60,6 +109,7 @@ INS** aarch_parse(char* filename, int* ret_sz) {
 
     if (!sscanf(line, "%x: %*s %s%n", &cur_ins->addr, cur_ins->op, &arg_start))
       continue;
+    cur_ins->type = findtype(cur_ins->op);
     
     char* arg_str = calloc((strlen(line) - arg_start + 1), sizeof(char));
     strncpy(arg_str, &line[arg_start], strlen(line) - arg_start + 1);
