2973fa7e607fd1392e2838d0ce01bd711284ca23
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-10-20 00:07:29 +0800

    Finish first pass of findbb and fix name conflict
    I used a hash map to look up the target of a branch/jump instruction
    in `ins_arr`. The size of a hash map is set to double of the next power
    of 2 of `ins_arr_sz` to keep the load factor always less equal than 0.5
    but greater than 0.25. Plus, I found that there are naming conflict
    accross code_analyzer.c and riscv_parser.c when linking, which is due to
    var `p` in both files.

--- code_analyzer.c
+++ code_analyzer.c
@@ -1,9 +1,10 @@
 #include "core.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 INS **ins_hash_map;  // Use a hash map for faster search
-int m, p;            // Size of the map, a power of 2, m = 1 << p;
+int m, pw;           // Size of the map, a power of 2, m = 1 << pw;
 
 /* Compute the next highest power of 2 of 32-bit x 
  * Check: https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
@@ -24,7 +25,7 @@ int next_p2(int x) {
  * means the fractional part of kA. m is the size of a the hash table.
  */
 int hash(unsigned long long key) {
-  return ((1 << p) - 1) & (((key * 2654435769) & 0xffffffff) >> (31 - p));
+  return ((1 << pw) - 1) & (((key * 2654435769) & 0xffffffff) >> (32 - pw));
 }
 
 /* Algorithm for finding basic blocks.
@@ -40,15 +41,47 @@ INS ***findbb(INS **ins_arr, int ins_arr_sz, int *ret_sz, int **ret_bb_sz) {
    * instruction 3. The instruction that follows a branch/jump instruction.
    */
   m = next_p2(ins_arr_sz) << 1;  // keep load facter < 0.5
-  p = ffs(m) - 1;  // find first bit
+  pw = ffs(m) - 1;  // find first bit
   ins_hash_map = calloc(m, sizeof(INS *));
 
+  ins_arr[0]->is_leader = 1;  // #1
+
+  int prev_type = NA;
   for (int i = 0; i < ins_arr_sz; ++i) {
-    ins_hash_map[hash(ins_arr[i]->addr)] = ins_arr[i];
+    int h = hash(ins_arr[i]->addr);
+
+    while (ins_hash_map[h] != NULL)
+      h = (h == m - 1)? 0 : h + 1;  // Linear probing
+
+    ins_hash_map[h] = ins_arr[i];
   }
 
-  ins_arr[0]->is_leader = 1;  // #1
-  // TODO: coding
+  for (int i = 0; i < ins_arr_sz; ++i) {
+    if (ins_arr[i]->type == BR) {
+      INS *cur_ins = ins_arr[i];
+      int follow = i + 1;
+      if (follow < ins_arr_sz) ins_arr[follow]->is_leader = 1;  // #3
+
+      if (!cur_ins->argc) continue;
+
+      int target_addr = strtol(cur_ins->argv[cur_ins->argc - 1], NULL, 16);
+      int h = hash(target_addr);
+
+      int cc = 0;
+      while (ins_hash_map[h]) {
+        INS *target = ins_hash_map[h];
+
+        if (target->addr == target_addr) {
+          printf("cc = %d\n", cc);
+          target->is_leader = 1;  // #2
+          break;
+        }
+
+        h = (h == m - 1)? 0 : h + 1;  // Another linear probing
+        ++cc;
+      }
+    }
+  }
 
   /* [Second Pass]
    * Starting from a leader, the set of all following instructions until and not
