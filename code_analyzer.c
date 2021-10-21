2fb1e3f4d5e8b618077269260e07e736bd4758cc
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-10-21 10:55:25 +0800

    Finish findbb function and use static functions
    Now the findbb function can read the INS* array, find all leaders in it,
    split it bb by bb, finally put them together in a list of bb. The size
    of each bb in the list is stored in an array of int called ret_bb_sz.
    The total number of bb's is stored in ret_sz.

--- code_analyzer.c
+++ code_analyzer.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-INS **ins_hash_map;  // Use a hash map for faster search
-int m, pw;           // Size of the map, a power of 2, m = 1 << pw;
+static INS **ins_hash_map;  // Use a hash map for faster search
+static int m, p;           // Size of the map, a power of 2, m = 1 << p;
 
 /* Compute the next highest power of 2 of 32-bit x 
  * Check: https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
  */
-int next_p2(int x) {
+static int next_p2(int x) {
   x--;
   x |= x >> 1;
   x |= x >> 2;
@@ -24,8 +24,8 @@ int next_p2(int x) {
  * h(k) = floor(m(kA % 1)), where A is a constant that 0 < A < 1, and (kA % 1)
  * means the fractional part of kA. m is the size of a the hash table.
  */
-int hash(unsigned long long key) {
-  return ((1 << pw) - 1) & (((key * 2654435769) & 0xffffffff) >> (32 - pw));
+static int hash(unsigned long long key) {
+  return ((1 << p) - 1) & (((key * 2654435769) & 0xffffffff) >> (32 - p));
 }
 
 /* Algorithm for finding basic blocks.
@@ -35,26 +35,28 @@ INS ***findbb(INS **ins_arr, int ins_arr_sz, int *ret_sz, int **ret_bb_sz) {
 
   if (!ins_arr_sz) return NULL;
 
+  m = next_p2(ins_arr_sz) << 1;  // keep load facter < 0.5 but > 0.25
+  p = ffs(m) - 1;                // find first bit
+  ins_hash_map = calloc(m, sizeof(INS *));
+
+  /* Put all INS reference into a hash map, for a faster searching for a target
+   * of a branch/jump instruction. Use linear probing to handle collision.
+   */
+  for (int i = 0; i < ins_arr_sz; ++i) {
+    int h = hash(ins_arr[i]->addr);
+
+    while (ins_hash_map[h] != NULL)
+      h = (h == m - 1)? 0 : h + 1;  // Linear probing to find a emtpy slot
+
+    ins_hash_map[h] = ins_arr[i];
+  }
+
   /* [Fisrt Pass]
    * Identify all the leader in the instruction list. There are three
    * categories: 1. The very first instruction 2. The target of a branch/jump
    * instruction 3. The instruction that follows a branch/jump instruction.
    */
-  m = next_p2(ins_arr_sz) << 1;  // keep load facter < 0.5
-  pw = ffs(m) - 1;  // find first bit
-  ins_hash_map = calloc(m, sizeof(INS *));
-
   ins_arr[0]->is_leader = 1;  // #1
-
-  int prev_type = NA;
-  for (int i = 0; i < ins_arr_sz; ++i) {
-    int h = hash(ins_arr[i]->addr);
-
-    while (ins_hash_map[h] != NULL)
-      h = (h == m - 1)? 0 : h + 1;  // Linear probing
-
-    ins_hash_map[h] = ins_arr[i];
-  }
 
   for (int i = 0; i < ins_arr_sz; ++i) {
     if (ins_arr[i]->type == BR) {
@@ -67,18 +69,15 @@ INS ***findbb(INS **ins_arr, int ins_arr_sz, int *ret_sz, int **ret_bb_sz) {
       int target_addr = strtol(cur_ins->argv[cur_ins->argc - 1], NULL, 16);
       int h = hash(target_addr);
 
-      int cc = 0;
       while (ins_hash_map[h]) {
         INS *target = ins_hash_map[h];
 
         if (target->addr == target_addr) {
-          printf("cc = %d\n", cc);
           target->is_leader = 1;  // #2
           break;
         }
 
         h = (h == m - 1)? 0 : h + 1;  // Another linear probing
-        ++cc;
       }
     }
   }
@@ -88,7 +87,53 @@ INS ***findbb(INS **ins_arr, int ins_arr_sz, int *ret_sz, int **ret_bb_sz) {
    * including the next leader is the basic block corresponding to the starting
    * leader. Thus every basic block has a leader.
    */
-  // TODO: coding
+  int leader = 0, max_ret_sz = 16, bb_idx = 0;
+  INS ***bb_list = malloc(max_ret_sz * sizeof(INS **));
+  int *bb_sz = malloc(max_ret_sz * sizeof(int));
+  while (leader < ins_arr_sz) {
+    int next_leader = leader + 1;
 
-  return NULL;
+    while (next_leader < ins_arr_sz && ins_arr[next_leader]->is_leader == 0)
+      ++next_leader;
+
+    int len = next_leader - leader;
+    INS **bb = malloc(len * sizeof(INS *));
+
+    for (int i = 0; i < len; ++i)
+      bb[i] = ins_arr[leader + i];  // copy ins from ins_arr to bb
+
+    if (bb_idx == max_ret_sz) {
+      max_ret_sz *= 2;
+      INS ***tmp_bb_list = realloc(bb_list, max_ret_sz * sizeof(INS **));
+      int *tmp_bb_sz = realloc(bb_sz, max_ret_sz * sizeof(int));
+      if (!tmp_bb_list) {
+        printf("Failed reallocating memory for size: %d\n", max_ret_sz);
+        break;
+      }
+      if (!tmp_bb_sz) {
+        printf("Failed reallocating memory for size: %d\n", max_ret_sz);
+        break;
+      }
+      bb_list = tmp_bb_list;
+      bb_sz = tmp_bb_sz;
+    }
+
+    bb_list[bb_idx] = bb;
+    bb_sz[bb_idx] = len;
+    ++bb_idx;
+
+    leader = next_leader;
+  } 
+
+  *ret_sz = bb_idx;
+  INS ***tmp_bb_list = realloc(bb_list, max_ret_sz * sizeof(INS **));  // shrink
+  int *tmp_bb_sz = realloc(bb_sz, max_ret_sz * sizeof(int));           // shrink
+  if (tmp_bb_list)
+    bb_list = tmp_bb_list;
+  if (tmp_bb_sz)
+    bb_sz = tmp_bb_sz;
+  *ret_bb_sz = bb_sz;
+
+  free(ins_hash_map);
+  return bb_list;
 }
